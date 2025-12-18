f70a06bc35e7744cdea3e0119c0b71ef333fc2c1
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2025-12-17 20:36:40 -0800

    added docs for production

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : "/CECS-448-Final-Project"
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/CECS-448-Final-Project'
+    }
+  }
 };
 
 export default config;
